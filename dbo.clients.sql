f29b5d36cb59f7a60b2d9b805bf0b8c45a0913e3
JOSE JAIME MARROQUIN SANCHEZ <jose.marroquin240633@liveusam.edu.sv>
2025-03-20 22:20:24 -0600

    Se añadio toda la actividad completa de la guia 6 y 7

---
+++ dbo.clients.sql
@@ -0,0 +1,13 @@
+﻿--Jose Jaime Marroquin Sanchez: 240633 
+
+CREATE TABLE [dbo].[clients] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [name]       VARCHAR (100) NOT NULL,
+    [email]      VARCHAR (150) NOT NULL,
+    [phone]      VARCHAR (20)  NULL,
+    [address]    VARCHAR (100) NULL,
+    [created_at] DATETIME      DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([email] ASC)
+);
+
